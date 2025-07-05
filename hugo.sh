234bfe57f3a78822e7f01527c303a886a4ee5ea5
derui <derutakayu@gmail.com>
2025-07-05 17:39:23 +0900

    feat: theme refine and content refine

--- hugo.sh
+++ hugo.sh
@@ -5,7 +5,7 @@ if [[ ! -d ./bin ]]; then
 fi
 
 if [[ ! -f ./bin/hugo ]]; then
-    curl -L -o bin/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.92.0/hugo_0.92.0_Linux-64bit.tar.gz
+    curl -L -o bin/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.147.9/hugo_0.147.9_Linux-64bit.tar.gz
     tar xf bin/hugo.tar.gz -C bin/
 fi
 
